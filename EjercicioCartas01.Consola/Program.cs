414783326663526a3d72f6554deab9d111185fe6
SantiagoMartin001 <SantiagoMartin001@users.noreply.github.com>
2023-07-04 18:42:35 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioCartas01.Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class EjercicioCarta01
+{
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        int suma = 0;
+        int nroCarta;
+
+        while (contador < 5)
+        {
+            Console.Write($"Ingrese el nro de la {contador + 1}° carta: ");
+            nroCarta = int.Parse(Console.ReadLine());
+
+            suma += nroCarta; // variable que oficia de acumulador
+            contador++; // variable que oficia de contador
+        }
+
+        Console.WriteLine($"La suma de las primeras cinco cartas es {suma}");
+    }
+}
